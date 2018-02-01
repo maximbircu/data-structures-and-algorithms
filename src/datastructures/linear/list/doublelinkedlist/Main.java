f59d11a3286dc833a4f7d5928e9a152020555730
maxim <maxim.bircu@jivygroup.com>
2018-02-01 18:39:29 +0200

    Started working on double linked list

---
+++ src/datastructures/linear/list/doublelinkedlist/Main.java
@@ -0,0 +1,7 @@
+package datastructures.linear.list.doublelinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/datastructures/linear/list/doublelinkedlist/Node.java
@@ -0,0 +1,11 @@
+package datastructures.linear.list.doublelinkedlist;
+
+class Node {
+    int data;
+    Node next;
+    Node previous;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
