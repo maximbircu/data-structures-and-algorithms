2f9ae9f9ca6435b7236fda35fd5e4e1bcc51a3d0
maxim <maxim.bircu@jivygroup.com>
2018-02-02 15:22:11 +0200

    Done with arraylist

---
+++ src/datastructures/linear/list/arraylist/Main.java
@@ -0,0 +1,21 @@
+package datastructures.linear.list.arraylist;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<Integer> test = new MyArrayList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(4);
+
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println(test.get(i));
+        }
+
+        test.remove(3);
+
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println(test.get(i));
+        }
+    }
+}
---
+++ src/datastructures/linear/list/arraylist/MyArrayList.java
@@ -0,0 +1,53 @@
+package datastructures.linear.list.arraylist;
+
+public class MyArrayList<E> {
+    Object[] data;
+
+    private int size = 0;
+    private int capacity = 1;
+
+    public MyArrayList() {
+        data = new Object[capacity];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        capacity = initialCapacity;
+        data = new Object[capacity];
+    }
+
+    public void add(E element) {
+        if (size == capacity - 1) {
+            extendCapacity();
+        }
+        data[size++] = element;
+    }
+
+    public E get(int index) {
+        checkRange(index);
+        return (E) data[index];
+    }
+
+    public void remove(int index) {
+        checkRange(index);
+        Object[] newArray = new Object[size - 1];
+        System.arraycopy(data, 0, newArray, 0, size - 1);
+        data[size--] = null;
+        data = newArray;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkRange(int index) {
+        if (index >= 0 && index >= size)
+            throw new IndexOutOfBoundsException();
+    }
+
+    private void extendCapacity() {
+        capacity *= 2;
+        Object[] newData = new Object[capacity];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
+}
